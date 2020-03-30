823aa61e9e4ecd11b8f8ca4477d326fce661c363
Justin Lin <ro0566320@gmail.com>
2020-03-29 23:40:59 -0400

    finish route

--- routes/api/book-routes.js
+++ routes/api/book-routes.js
@@ -1,16 +1,31 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+const {getSavedBooks, saveBook, removeBook} = require('../../controllers/book-controller')
 
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
 
-module.exports = router;
+// GET and POST at /api/books
+router.routes('/').get(getSavedBooks).post(saveBook)
+
+// DELETE at /api/books/:id
+router.route('/:id').delete(removeBook)
+
+module.exports = router
+
+
+
+
+// const booksController = require("../../controllers/booksController");
+
+// // Matches with "/api/books"
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create);
+
+// // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   .get(booksController.findById)
+//   .put(booksController.update)
+//   .delete(booksController.remove);
+
+// module.exports = router;
