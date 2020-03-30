c00a2c75d3e748832a4f2062f35fad0b132cadb6
Justin Lin <ro0566320@gmail.com>
2020-03-29 23:55:54 -0400

    finish API.js in utils directory connect to back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,53 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import Nav from "./components/Nav";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Saved from "./pages/saved";
 
-function App() {
-  return (
+const App = () => {
+  return(
     <Router>
-      <div>
-        <Nav />
+      <React.Fragment>
         <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
+          <Route exact path = "/" component = {Search} />
+          <Route exact path = "/search" component = {Search} />
+          <Route exact path = "/saved" component = {Saved} />
+          <Route component = {NoMatch} />
         </Switch>
-      </div>
+      </React.Fragment>
     </Router>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch> 
+//           <Route exact path={["/", "/books"]}>
+//             <Books />
+//           </Route>
+//           <Route exact path="/books/:id">
+//             <Detail />
+//           </Route>
+//           <Route>
+//             <NoMatch />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
